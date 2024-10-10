66574d8bde26101f2bae4af0c109f18769929df1
sloraris <97694636+sloraris@users.noreply.github.com>
2024-10-09 22:09:21 -0600

    changed site title and description metadata

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'sloraris | Personal Portfolio Website';
-export const SITE_DESCRIPTION = 'Welcome to sloraris.dev, where I pretend I have everything under control—one YAML file at a time. I’m Parker Owings, and while I’m not building the next social media empire, I am deep in the trenches of networking, DevOps, and turning my homelab into a glorified experiment zone. Whether I’m automating my servers or trying not to break my home network (again), this is where I share my misadventures and occasional successes. Come for the portfolio, stay for the troubleshooting logs.';
+export const SITE_TITLE = 'sloraris | A Multipurpose Exhibition';
+export const SITE_DESCRIPTION = 'Welcome to sloraris.dev, where I pretend to know what I\'m talking about. Here you\'ll find a collection of my projects, thoughts, experiences, and the obscure things I\'ve had to Google to solve my niche problems. Come for the portfolio, stay for the sarcasm.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
